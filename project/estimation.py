11975c9101c1768a802b79fbe4099ea3fdc01a7a
wtrr59 <wtrr59@naver.com>
2020-07-17 23:02:01 +0900

    Tracking 정량평가를 위한 GT와 Track Result 매칭 알고리즘 #16

--- project/estimation.py
+++ project/estimation.py
@@ -15,25 +15,22 @@ def arg_parse():
     """
     parser = argparse.ArgumentParser(description='Estimation')
     parser.add_argument("--threshold", dest='threshold', help="Estimation distance threshold", default=1/5, type=float)
-    parser.add_argument("--images", dest='images', help="Image / Directory containing images to perform detection upon",default="./MOT17-03-DPM/img1/", type=str)
+    parser.add_argument("--images", dest='images', help="Image / Directory containing count to perform detection upon",default="./MOT17-03-DPM/img1/", type=str)
     parser.add_argument("--reso", dest='reso', help="Input resolution of the network. Increase to increase accuracy. Decrease to increase speed", default="416", type=str)
     return parser.parse_args()
 
 
 def write_result(x, img):
-    write_object = x[1]
-    left_x = float(write_object[1]) - float(write_object[3])/2
-    left_y = float(write_object[2]) - float(write_object[4])/2
-    right_x = float(write_object[1]) + float(write_object[3])/2
-    right_y = float(write_object[2]) + float(write_object[4])/2
-    id = int(float(x[1][5]))
+    print(x)
+    write_object = x[0]
+    id = int(float(write_object[1]))
 
-    track_object = torch.from_numpy(np.array([left_x, left_y, right_x, right_y]))
+    track_object = torch.from_numpy(np.array([float(write_object[2]), float(write_object[3]), float(write_object[4]), float(write_object[5])]))
 
     c1 = tuple(track_object[0:2].int())
     c2 = tuple(track_object[2:4].int())
 
-    label = "track"+str(int(id))
+    label = "GT"+str(int(id))
 
     color = colors[id % 14]
 
@@ -44,19 +41,15 @@ def write_result(x, img):
     cv2.rectangle(img, c1, c2, color, -1)
     cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [0,0,0], 1)
 
-    write_object = x[0]
-    left_x = float(write_object[1]) - float(write_object[3]) / 2
-    left_y = float(write_object[2]) - float(write_object[4]) / 2
-    right_x = float(write_object[1]) + float(write_object[3]) / 2
-    right_y = float(write_object[2]) + float(write_object[4]) / 2
-    id = int(float(x[1][5]))
+    write_object = x[1]
+    id = int(float(write_object[1]))
 
-    track_object = torch.from_numpy(np.array([left_x, left_y, right_x, right_y]))
+    track_object = torch.from_numpy(np.array([float(write_object[2]), float(write_object[3]), float(write_object[4]), float(write_object[5])]))
 
     c1 = tuple(track_object[0:2].int())
     c2 = tuple(track_object[2:4].int())
 
-    label = "MOT"+str(int(id))
+    label = "Track"+str(int(id))
 
     color = colors[id % 14]
 
@@ -69,36 +62,17 @@ def write_result(x, img):
     return img
 
 
+# [frameNo , ID , left_X , left_Y , right_X , right_Y , mid_X , mid_Y , width , height]
+# [   0    , 1  ,   2    ,   3    ,    4    ,    5    ,   6   ,   7   ,   8   ,   9   ]
+def convert_to_track_array(line):
+    right_X = float(line[2]) + float(line[4])
+    right_Y = float(line[3]) + float(line[5])
+    mid_X = float(line[2]) + (float(line[4]) / 2)
+    mid_Y = float(line[3]) + (float(line[5]) / 2)
+    width = float(line[4])
+    height = float(line[5])
 
-# Frame 출력 함수
-def printFrame(frame):
-
-    frameSize = frame.__len__()
-    frameCount = 0
-
-    while frameCount < frameSize:
-        print("frame"+str(frame[frameCount][0][0])+": ")
-
-        objSize = frame[frameCount].__len__()
-        objCount = 0
-
-        while objCount < objSize:
-            print("          "+str(frame[frameCount][objCount]))
-            objCount += 1
-
-        frameCount += 1
-
-
-# change line
-# [frameNo , ID , leftCoordinate , topCoordinate , width , height , conf , x , y , z]
-# to
-# [frameNo, midPointX, midPointY , width, height]
-def convert_to_midPoint(line):
-
-    midPointX = float(line[2]) + (float(line[4]) / 2)
-    midPointY = float(line[3]) + (float(line[5]) / 2)
-
-    parsedLine = [line[0], midPointX, midPointY, line[4], line[5]]
+    parsedLine = [line[0], line[1], line[2], line[3], right_X, right_Y, mid_X, mid_Y, width, height]
     return parsedLine
 
 
@@ -107,28 +81,66 @@ def convert_to_midPoint(line):
 #                     [ [MOT result[3]] , [track_result[4]] ]
 #                                       ...
 #                    ]
+# def compare_results(track_result, MOT_result, threshold):
+#     matching_result = []
+#     matched_list = []
+#
+#     # frame loop
+#     for i in range(1, MOT_result.__len__()):
+#         matching_result.append([])
+#         # MOT object loop
+#         for MOT_object in MOT_result[i]:
+#             # track object loop
+#             for track_object in track_result[i]:
+#                 distance_X = abs(float(track_object[6]) - float(MOT_object[6]))
+#                 distance_Y = abs(float(track_object[7]) - float(MOT_object[7]))
+#
+#                 if float(MOT_object[8]) * threshold >= distance_X and float(MOT_object[9]) * threshold >= distance_Y:
+#                     matching_result[i-1].append([MOT_object, track_object])
+#                     matched_list.append(track_object)
+#                     break
+#
+#             for pop_object in matched_list:
+#                 track_result[i].remove(pop_object)
+#             matched_list.clear()
+#
+#     return matching_result
+
 def compare_results(track_result, MOT_result, threshold):
     matching_result = []
-    matched_list = []
 
     # frame loop
-    for i in range(1, MOT_result.__len__()):
+    for i in range(1, track_result.__len__()):
         matching_result.append([])
         # MOT object loop
-        for MOT_object in MOT_result[i]:
+        for track_object in track_result[i]:
             # track object loop
-            for track_object in track_result[i]:
-                distance_X = abs(float(track_object[1]) - float(MOT_object[1]))
-                distance_Y = abs(float(track_object[2]) - float(MOT_object[2]))
+            max_iou = 0
+            max_iou_object = None
 
-                if float(MOT_object[3]) * threshold >= distance_X and float(MOT_object[4]) * threshold >= distance_Y:
-                    matching_result[i-1].append([MOT_object, track_object])
-                    matched_list.append(track_object)
-                    break
+            for MOT_object in MOT_result[i]:
+                xA = max(float(MOT_object[2]), float(track_object[2]))
+                yA = max(float(MOT_object[3]), float(track_object[3]))
+                xB = min(float(MOT_object[4]), float(track_object[4]))
+                yB = min(float(MOT_object[5]), float(track_object[5]))
 
-            for pop_object in matched_list:
-                track_result[i].remove(pop_object)
-            matched_list.clear()
+                interArea = max(0, xB - xA)*max(0, yB - yA)
+
+                if interArea == 0:
+                    continue
+
+                boxAArea = MOT_object[8] * MOT_object[9]
+                boxBArea = track_object[8] * track_object[9]
+
+                iou = interArea / float(boxAArea + boxBArea - interArea)
+
+                if iou > max_iou:
+                    max_iou = iou
+                    max_iou_object = MOT_object
+
+            if max_iou_object != None:
+                matching_result[i - 1].append([max_iou_object, track_object])
+                MOT_result[i].remove(max_iou_object)
 
     return matching_result
 
@@ -139,7 +151,7 @@ if __name__ == '__main__':
     images = args.images
 
 ####################### Track MOT Parsing #######################
-    trackFile = open('trackResult.txt', 'r')
+    trackFile = open('./MOT_result/trackResult.txt', 'r')
     track_result = [[], []]
 
     curFrame = 1
@@ -157,52 +169,46 @@ if __name__ == '__main__':
             curFrame += 1
             track_result.append([])
 
-        track_result[curFrame].append(parsed_line)
+        converted_line = convert_to_track_array(parsed_line)
+        track_result[curFrame].append(converted_line)
 
     trackFile.close()
-    trackFrame = curFrame
 
-####################### MOT 17 Det Parsing #######################
-    detFile = open('./MOT17-03-DPM/det/det.txt', 'r')
-    mot_result = [[], []]
+####################### MOT 17 GT Parsing #######################
+    gtFile = open('./MOT17-04-DPM/gt/gt.txt', 'r')
+    gt_result = [[]]
+    count = curFrame
 
-    curFrame = 1
+    for frame in range(0, count):
+        gt_result.append([])
 
     while True:
-        line = detFile.readline()
+        line = gtFile.readline()
 
         if line == '':
             break
 
         line = line.split("\n")[0]
         parsed_line = line.split(",")
-        converted_line = convert_to_midPoint(parsed_line)
 
-        if curFrame != float(parsed_line[0]):
-            curFrame += 1
-            # track_result의 Frame 개수 초과 시 break
-            if curFrame > trackFrame:
-                break
+        if int(parsed_line[0]) > count:
+            continue
 
-            mot_result.append([])
+        converted_line = convert_to_track_array(parsed_line)
+        gt_result[int(converted_line[0])].append(converted_line)
 
-        mot_result[curFrame].append(converted_line)
-
-    detFile.close()
+    gtFile.close()
 
 ########################## Compare Result ##########################
-    output_list = compare_results(track_result, mot_result, threshold)
+    output_list = compare_results(track_result, gt_result, threshold)
 
 ########################## Write Result ##########################
-
-    for img_id in range(trackFrame):
-
-        inp_dim = int(args.reso)
-        assert inp_dim % 32 == 0
-        assert inp_dim > 32
+    print(count)
+    for img_id in range(count):
+        inp_dim = 416
 
         curr_img_num_str = str(img_id + 1).zfill(6) + ".jpg"
-        curr_img_path = osp.join(osp.realpath('.'), images, curr_img_num_str)
+        curr_img_path = osp.join(osp.realpath('.'), "./MOT17-04-DPM/img1/", curr_img_num_str)
         processed_img, curr_img, dim = prep_image(curr_img_path, inp_dim)
 
         list(map(lambda x: write_result(x, curr_img), output_list[img_id]))
